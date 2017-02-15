ae7544162bef0fdc25bd2acd183f6f2d4d15886a
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-02-15 17:11:53 -0500

    i actually did it bro yaaaassss

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+	  $(".signin").click(function() {
+	    $("div.modal").fadeIn("div.modal");
+	  });
+	  $("button.close").click(function() {
+	    $("div.modal").fadeOut("div.modal");
+	  });
+	  $("button.submit").click(function() {
+	    $("input").addClass("error");
+	  });
+	  $("input").click(function() {
+	    $(this).removeClass("error");
+	  });
+	});
