817ba7551d06961631b6f648430669505e319a03
Paul Mohabir <Paul.Mohabir88@gmail.com>
2017-02-15 17:15:55 -0500

    put some comments in cause i forgot to

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@ $(function(){
 
 	  $(".signin").click(function() {
 	    $("div.modal").fadeIn("div.modal");
-	  });
+	  }); //bring up the sign in box
+
 	  $("button.close").click(function() {
 	    $("div.modal").fadeOut("div.modal");
-	  });
+	  }); //close the sign in box
+
 	  $("button.submit").click(function() {
 	    $("input").addClass("error");
-	  });
+	  }); //hit submit but no info entered resulting in an error
+
 	  $("input").click(function() {
 	    $(this).removeClass("error");
-	  });
-	});
+	  }); // remove error if there is an input ....this is the buttom submit 
+
+	}); //close functon
